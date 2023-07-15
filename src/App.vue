b0c0d417a7b916e82d1795b7a2c36d8bf9330929
DESKTOP-JS53CE0\Shutter <6331335921@student.chula.ac.th>
2023-07-15 20:04:08 +0700

    learned about refs property

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click Me</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "My First Vue App :)",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      // this.$refs.name.classList.add("active");
+      // this.$refs.name.focus();
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +30,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
